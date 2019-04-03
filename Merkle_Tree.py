de2426cdfeba631b688d167e2ac6826e3412dbc7
BulkyPwn <41984115+BulkyPwn@users.noreply.github.com>
2019-04-03 16:27:43 +0800

    Add files via upload

--- Merkle_Tree.py
+++ Merkle_Tree.py
@@ -19,22 +19,25 @@ def create_root():
 def create_node(node_id,node_data,left,right):
     return MerkleTree_node(id=node_id,data=node_data,lhash=left,rhash=right)
 
-def update_one_node(id,mt,flag):
+def update_one_node(id,mt,flag,mask):
     import pymongo
     conn = pymongo.MongoClient()
     db = conn.test
-    handler = db.mt
+    handler = db.packing_mt
 
     updating_node_id = id
-    print("本次更新id为 "+str(id))
+    if mask == 0 :
+        print("本次更新id为 "+str(id))
     if len(mt.nodes) > id * 2 + 1 :
         for record in handler.find({"_id":updating_node_id * 2 +1}):
             updating_lhash = record["Hash"]
-            print("更新结点LHash为 "+updating_lhash)
+            if mask == 0:
+                print("更新结点LHash为 "+updating_lhash)
         if len(mt.nodes) > id * 2 + 2:
             for record in handler.find({"_id": updating_node_id * 2 + 2}):
                 updating_rhash = record["Hash"]
-                print("更新结点RHash为 "+updating_rhash)
+                if mask == 0:
+                    print("更新结点RHash为 "+updating_rhash)
         else:
             updating_rhash = ""
     else:
@@ -43,86 +46,112 @@ def update_one_node(id,mt,flag):
 
     for record in handler.find({"_id": id}):
         hashing_data = record["Data"]
-        print("更新节点Data为 "+hashing_data)
+        if mask == 0 :
+            print("更新节点Data为 "+hashing_data)
 
     m = hashlib.sha256()
     m.update(str(hashing_data + updating_lhash + updating_rhash).encode("utf-8"))
     updating_hash = m.hexdigest()
 
     handler.update_one({"_id": id},{ "$set":{"LHash": updating_lhash, "RHash": updating_rhash, "Hash": updating_hash}})
-    if(flag == 0):
+    if(flag == 0 & mask == 0):
         print("更新成功！(共更新 1 条记录)")
 
 
-def update_one_way_of_one_node(id,mt,flag):
+def update_one_way_of_one_node(id,mt,flag,mask):
     updating_id = id
     count = 0
-    mask = 0
+    halt = 0
     while updating_id >= 0:
-        update_one_node(updating_id,mt,1)
+        update_one_node(updating_id,mt,1,mask)
         count+=1
         updating_id = int((updating_id-1)/2)
-        if (updating_id == 0) & (mask == 1):
+        if (updating_id == 0) & (halt == 1):
             updating_id = -1
         if updating_id == 0:
-            mask = 1
-    if(flag == 0):
+            halt = 1
+    if(flag == 0 & mask == 0):
         print("更新成功！(共更新 "+str(count)+" 条记录)")
 
-def update_all_tree(mt):
+def update_all_tree(mt,mask):
     i = len(mt.nodes) - 1
     count = 0
     while i>=0:
-        update_one_way_of_one_node(i, mt,1)
+        update_one_way_of_one_node(i, mt,1,mask)
         i -= 1
         count+=1
-    print("更新成功！(共更新 "+str(count)+" 条记录)")
+    if mask == 0:
+        print("更新成功！(共更新 "+str(count)+" 条记录)")
 
-def insert_one_node(mt):
+def insert_one_node(mt,mask):
     import pymongo
     conn = pymongo.MongoClient()
     db = conn.test
-    handler = db.mt
+    handler = db.packing_mt
 
     n = mt.nodes[len(mt.nodes) - 1]
     handler.insert_one({"_id": n.id,"Data":n.data,"LHash":n.lhash,"RHash":n.rhash,"Hash":n.hash})
-    print("id:{}".format(n.id))
-    print("Data:{}".format(n.data))
-    print("LHash:{}".format(n.lhash))
-    print("RHash:{}".format(n.rhash))
-    print("Hash:{}".format(n.hash))
-    print("\n")
+    if mask == 0:
+        print("id:{}".format(n.id))
+        print("Data:{}".format(n.data))
+        print("LHash:{}".format(n.lhash))
+        print("RHash:{}".format(n.rhash))
+        print("Hash:{}".format(n.hash))
+        print("\n")
 
-    print("哈希值插入成功！")
+        print("哈希值插入成功！")
 
 class merkle_t:
-    def __init__(self):
+    def __init__(self,mask):
         self.nodes = [create_root()]
-        insert_one_node(self)
+        insert_one_node(self,mask)
 
-    def add_node(self,node_data,left,right):
+    def add_node(self,node_data,left,right,mask):
         new_node = create_node(len(self.nodes),node_data,left,right)
         self.nodes.append(new_node)
-        insert_one_node(self)
+        insert_one_node(self,mask)
         return new_node
 
+def drop_packing_mt():
+    import pymongo
+    conn = pymongo.MongoClient()
+    db = conn.test
+    handler = db.packing_mt
+    handler.drop()
 
+def update_packing_mt(node_datas,mask):
+    # mt = merkle_t()
+    # n1 = mt.add_node(node_data="Jack send 0.3 BTC to Alice", left="", right="")  # 添加一个结点
+    # n2 = mt.add_node(node_data="Jack send 0.5 BTC to Bob", left="", right="")
+    # n3 = mt.add_node(node_data="Bob send 0.5 BTC to Bob", left="", right="")
+    # n4 = mt.add_node(node_data="Tom send 0.2 BTC to Bob", left="", right="")
+    # n5 = mt.add_node(node_data="Bob send 0.3 BTC to Alice", left="", right="")
+    # n6 = mt.add_node(node_data="3303:Analysis Succeeded", left="", right="")
+    # n7 = mt.add_node(node_data="1024:Analysis Failed", left="", right="")
+
+    drop_packing_mt()
+
+    mt = merkle_t(mask)
+    n1 = mt.add_node(node_data=node_datas[0], left="", right="",mask=mask)  # 添加一个结点
+    n2 = mt.add_node(node_data=node_datas[1], left="", right="",mask=mask)
+    n3 = mt.add_node(node_data=node_datas[2], left="", right="",mask=mask)
+    n4 = mt.add_node(node_data=node_datas[3], left="", right="",mask=mask)
+    n5 = mt.add_node(node_data=node_datas[4], left="", right="",mask=mask)
+    n6 = mt.add_node(node_data=node_datas[5], left="", right="",mask=mask)
+    n7 = mt.add_node(node_data=node_datas[6], left="", right="",mask=mask)
+    if mask == 0:
+        print("最后一个结点的序号为 " + str(len(mt.nodes) - 1))
+        print("\n")
+
+    update_all_tree(mt,mask)
+
+    if mask:
+        print("当前操作区块的默克尔树已更新！")
 
 def main():
-    mt = merkle_t()
-    n1 = mt.add_node(node_data="Jack send 0.3 BTC to Alice",left="",right="")#添加一个结点
-    n2 = mt.add_node(node_data="Jack send 0.5 BTC to Bob",left="",right="")
-    n3 = mt.add_node(node_data="Bob send 0.5 BTC to Bob", left="", right="")
-    n4 = mt.add_node(node_data="Tom send 0.2 BTC to Bob", left="", right="")
-    n5 = mt.add_node(node_data="Bob send 0.3 BTC to Alice", left="", right="")
-    n6 = mt.add_node(node_data="3303:Analysis Succeeded", left="", right="")
-    n7 = mt.add_node(node_data="1024:Analysis Failed", left="", right="")
+    node_datas = ["Jack send 0.3 BTC to Alice","Jack send 0.5 BTC to Bob","Bob send 0.5 BTC to Bob","Tom send 0.2 BTC to Bob","Bob send 0.3 BTC to Alice","3303:Analysis Succeeded","1024:Analysis Failed"]
 
-    print("最后一个结点的序号为 "+ str(len(mt.nodes) - 1))
-    print("\n")
-
-    update_all_tree(mt)
-
+    update_packing_mt(node_datas,0)
 
     # import pymongo
     #
--- blockchain.py
+++ blockchain.py
@@ -6,24 +6,28 @@ class Block:
     #     data
     #     timestamp
     #     hash
-    def __init__(self,data,prev_hash):
+    def __init__(self,data,prev_hash,root_hash):
         self.previous_hash = prev_hash
         self.data = data
+        self.root_hash = root_hash
+        self.h = ""
         self.nonce = ""
 
     #计算区块的哈希值
     @property
     def hash(self):
         message = hashlib.sha256()
+        message.update(str(self.previous_hash).encode("utf-8"))
         message.update(str(self.data).encode("utf-8"))
+        message.update(str(self.root_hash).encode("utf-8"))
         message.update(str(self.nonce).encode("utf-8"))
         digest = message.hexdigest()
-        return message.hexdigest()
+        return digest
 
 
 
 def create_genesis_block():
-    return Block(data="Genesis Block",prev_hash="")
+    return Block(data="Genesis Block",prev_hash="",root_hash="")
 
 class BlockChain:
     #区块链结构体
@@ -31,15 +35,31 @@ class BlockChain:
     def __init__(self):
         self.blocks = [create_genesis_block()]#初始化创建 创世区块
 
-    def add_block(self,data):
+    def add_block(self,data,node_datas):
         #添加区块
         #param block
         #return:
-        prev_block = self.blocks[len(self.blocks)-1]
-        new_block = Block(data,prev_block.hash)
+
+        import sys
+        sys.path.append("D:\Python Projects/venv/")
+        import Merkle_Tree
+        Merkle_Tree.update_packing_mt(node_datas,1)
+
+        if len(self.blocks)>0:
+            prev_block = self.blocks[len(self.blocks)-1]
+        import pymongo
+        conn = pymongo.MongoClient()
+        db = conn.test
+        handler = db.packing_mt
+        root_hash = handler.find({"_id":0})[0]["Hash"]
+        if len(self.blocks) > 0:
+            new_block = Block(data,prev_block.hash,root_hash)
+        else:
+            new_block = Block(data,tail_hash, root_hash)
         pow = ProofOfWork(new_block)
         nonce,digest = pow.mine()
         new_block.nonce = nonce
+        new_block.h = digest
         self.blocks.append(new_block)
         return new_block
 
@@ -51,14 +71,16 @@ class ProofOfWork():
     def mine(self):# 出块时间：约为0.001001second
         #挖矿函数
         i = 0
-        prefix = "3"
+        prefix = "0000"
         # import datetime
         # start_time = datetime.datetime.now()
 
         while True:
             nonce = str(i)
             message = hashlib.sha256()
+            message.update(str(self.block.previous_hash).encode("utf-8"))
             message.update(str(self.block.data).encode("utf-8"))
+            message.update(str(self.block.root_hash).encode("utf-8"))
             message.update(nonce.encode("utf-8"))
             digest = message.hexdigest()
             if digest.startswith(prefix):
@@ -66,21 +88,90 @@ class ProofOfWork():
                 # print(end_time - start_time)
                 return nonce,digest
             i += 1
-            print(i)
+            # print(i)
 
+def drop_blockchain():
+    import pymongo
+    conn = pymongo.MongoClient()
+    db = conn.test
+    handler = db.blockchain
+    handler.drop()
 
+def test_product_one_block_time():
+    import datetime #计算出块时间：出块时间约为 5.595643s
+    start_time = datetime.datetime.now()
+
+    node_datas = ["Jack send 0.3 BTC to Alice", "Jack send 0.5 BTC to Bob", "Bob send 0.5 BTC to Bob",
+                  "Tom send 0.2 BTC to Bob", "Bob send 0.3 BTC to Alice", "3303:Analysis Succeeded",
+                  "1024:Analysis Failed"]  # 块内 默克尔树 存储数据
+    bc.add_block("Alice gives her key to Bob", node_datas)  # 块头 存储数据
+
+    end_time = datetime.datetime.now()
+    print(end_time - start_time)
 
 def main():
+    #drop_blockchain()
+
     bc = BlockChain()
 
-    b1 = bc.add_block("Jack send 0.3 BTC to Alice")
-    # b2 = bc.add_block("Alice send 0.1 BTC to Tom")
+    ##判断该链是否为空：若为空，新建bc；若不空，则获取链尾哈希
+    import pymongo
+    conn = pymongo.MongoClient()
+    db = conn.test
+    handler = db.blockchain.find()
+    try:
+        if(handler[0]["_id"] != ""):
+            bc.blocks = []
+            global tail_hash
+            tail_hash = db.blockchain.find().sort("_id",pymongo.DESCENDING)[0]["Hash"]
+    except:
+        print("区块链为空")
+
+    data = "ALOHA"
+    node_datas = ["Data1", "Data2", "Data3",
+                  "Data4", "Data5", "Data6",
+                  "Data7"]
+
+    bc.add_block(data,node_datas)
+
+
+    # ##需手动设置 node_datas 、 bc.add_block(data,node_datas)##
+    # node_datas = ["Jack send 0.3 BTC to Alice", "Jack send 0.5 BTC to Bob", "Bob send 0.5 BTC to Bob",
+    #               "Tom send 0.2 BTC to Bob", "Bob send 0.3 BTC to Alice", "3303:Analysis Succeeded",
+    #               "1024:Analysis Failed"]#块内 默克尔树 存储数据
+    # b1 = bc.add_block("Alice gives her key to Bob",node_datas)#块头 存储数据
+    #
+    # node_datas = ["Data1", "Data2", "Data3",
+    #               "Data4", "Data5", "Data6",
+    #               "Data7"]
+    # b2 = bc.add_block("Winter is coming.",node_datas)
+
+    # node_datas = ["Jack send 0.3 BTC to Alice", "Jack send 0.5 BTC to Bob", "Bob send 0.5 BTC to Bob",
+    #               "Tom send 0.2 BTC to Bob", "Bob send 0.3 BTC to Alice", "3303:Analysis Succeeded",
+    #               "1024:Analysis Failed"]
+    # b3 = bc.add_block("Valar Morghulis",node_datas)
+    #
+    # node_datas = ["Jack send 0.3 BTC to Alice", "Jack send 0.5 BTC to Bob", "Bob send 0.5 BTC to Bob",
+    #               "Tom send 0.2 BTC to Bob", "Bob send 0.3 BTC to Alice", "3303:Analysis Succeeded",
+    #               "1024:Analysis Failed"]
+    # b4 = bc.add_block("Valar Dohaeris",node_datas)
+
+    import pymongo
+
+    conn = pymongo.MongoClient()
+    db = conn.test
 
     for b in bc.blocks:
+        handler = db.blockchain
+        handler.insert_one({"Prev_Hash": b.previous_hash,"Data":b.data,"Hash":b.hash,"Root_Hash":b.root_hash,"Nonce":b.nonce})
         print("Prev Hash:{}".format(b.previous_hash))
         print("Data:{}".format(b.data))
         print("Hash:{}".format(b.hash))
+        print("Root_Hash:{}".format(b.root_hash))
+        print("Nonce:{}".format(b.nonce))
         print("\n")
+
+    print("新区块插入成功！")
 
     # b1.data = "Jack send 1.3 BTC to Alice"#篡改b1区块的data
     #
@@ -95,4 +186,3 @@ def main():
     #     print("\n")
 
 main()
-
